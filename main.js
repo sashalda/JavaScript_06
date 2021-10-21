8794406dacde414a811fb0b4cf5608e72d5b5dd9
sashalda <sdod.26@gmail.com>
2021-10-21 19:44:29 -0300

    arreglitos

--- main.js
+++ main.js
@@ -1,49 +1,78 @@
-const hora = 50;
-class Libro {
-    constructor(nombre, paginas, autor) {
-        this.nombre = nombre;
-        this.paginas = paginas;
-        this.autor = autor;
+const libros = [
+    {
+        nombre: "el principito",
+        autor: "antoine de saint-exupéry"
+    }, {
+        nombre: "el psicoanalista",
+        autor:"John Katzenbach",
+    }, {
+        nombre: "la biblia",
+        autor: "la iglesia",
+    }, {
+        nombre: "los tres chanchitos",
+        autor: "Los hermanos Grimm",
     }
+]
 
-    obtenerDificultad() {
-        let dificultad = 0;
+const nombreLibro = prompt('ingrese el nombre de su libro')
 
-        switch(this.nombre) {
-            case "el principito":
-                dificultad = 1.0;
-                break;
-            case "el psicoanalista":
-                dificultad = 1.8;
-                break;
-            case "la biblia":
-                dificultad = 2.2;
-                break;
-            case "los tres chanchitos":
-                dificultad = 1.2;
-                break;    
-            default:
-                alert("ERROR");
-        }
-
-        let calculo = (this.paginas/hora)*dificultad;
-        console.log("El tiempo de lectura es de "+ calculo + " horas");
-
+for (let i = 0; i < libros.length; i++ ) {
+    if (nombreLibro === libros[i].nombre) {
+        alert("Has elegido "+ libros[i].nombre)
     }
 }
 
-const libro1 = new Libro("el principito", 200,  "Antoine de Saint-Exupéry");
-libro1.obtenerDificultad();
-console.log(libro1);
+const nombreAutor = prompt('ingrese el autor')
 
-const libro2 = new Libro("el psicoanalista", 600, "John Katzenbach");
-libro2.obtenerDificultad();
-console.log(libro2);
+for (let i = 0; i < libros.length; i++ ) {
+    if (nombreAutor === libros[i].autor) {
+        alert("Has elegido "+ libros[i].autor)
+    }
+}
 
-const libro3 = new Libro("la biblia", 3000, "Iglesia");
-libro3.obtenerDificultad();
-console.log(libro3);
+ 
+function pedirPaginas() {
+      return parseInt(prompt('Ingrese la cantidad de páginas'));
+  }
+ 
+let paginas = pedirPaginas();
 
-const libro4 = new Libro("los tres chanchitos", 50, "Los hermanos Grimm");
-libro4.obtenerDificultad();
-console.log(libro4);
+const hora = 50;
+
+class Libro {
+     constructor(nombre, paginas, autor) {
+         this.nombre = nombre;
+         this.paginas = paginas;
+         this.autor = autor;
+     }
+
+     obtenerDificultad() {
+         let dificultad = 0;
+
+         switch(this.nombre) {
+             case "el principito":
+                 dificultad = 1.0;
+                 break;
+             case "el psicoanalista":
+                 dificultad = 1.8;
+                 break;
+             case "la biblia":
+                 dificultad = 2.2;
+                 break;
+             case "los tres chanchitos":
+                 dificultad = 1.2;
+                 break;    
+             default:
+                 alert("ERROR");
+         }
+
+         let calculo = (this.paginas/hora)*dificultad;
+         console.log("El tiempo de lectura es de "+ calculo + " horas");
+
+     }
+ }
+
+ const libroX = new Libro(nombreLibro, paginas, nombreAutor) 
+ console.log(libroX)
+ libroX.obtenerDificultad();
+ console.log(libroX);
